1afc490f79acb2b1fc98fa085efd43dbc0dab563
Your Name <huyhuhyhoa@gmail.com>
2023-12-28 11:35:09 +0700

    ref #1 DEV: fix login with third party

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    uid {Faker::Number.number(digits: 6) }
+    uid { Faker::Number.number(digits: 6) }
     provider { Faker::Omniauth.google[:provider] }
 
-    name {Faker::Name.name  }
-    email {Faker::Internet.email }
+    name { Faker::Name.name  }
+    email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 6) }
   end
 end
