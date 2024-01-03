9ce6431886329f893b44a26ad85baa48ad735d51
Your Name <huyhuhyhoa@gmail.com>
2024-01-03 16:46:02 +0700

    ref #1 DEV: fix rspec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,6 +2,13 @@
 
 require 'rails_helper'
 
+RSpec.describe User, type: :model do
+  it 'should have a valid factory' do
+    user = FactoryBot.create(:user)
+    expect(user).to be_valid
+  end
+end
+
 RSpec.describe SessionsController, type: :controller do
   it 'should have a valid factory' do
     user = FactoryBot.create(:user)
@@ -36,60 +43,59 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe 'GET #omniauth' do
-
     it 'login with third party' do
-      user = FactoryBot.create(:user) 
+      user = FactoryBot.create(:user)
       allow(User).to receive(:new).and_return(user)
       OmniAuth.config.test_mode = true
       OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-                                                                         'provider' => 'google_oauth2',
-                                                                         'uid' => '111111111111111111',
-                                                                         'info' => {
-                                                                           'nick_name' => 'John Smith',
-                                                                           'name' => 'John Smith',
-                                                                           'email' => 'john@example.com',
-                                                                           'first_name' => 'John',
-                                                                           'last_name' => 'Smith',
-                                                                           'image' => 'https://lh4.googleusercontent.com/photo.jpg',
-                                                                           'urls' => {
-                                                                             'google' => 'https://plus.google.com/+JohnSmith'
-                                                                           }
-                                                                         },
-                                                                         'credentials' => {
-                                                                           'token' => 'TOKEN',
-                                                                           'refresh_token' => 'REFRESH_TOKEN',
-                                                                           'expires_at' => 1_496_120_719,
-                                                                           'expires' => true
-                                                                         },
-                                                                         'extra' => {
-                                                                           'id_token' => 'ID_TOKEN',
-                                                                           'id_info' => {
-                                                                             'azp' => 'APP_ID',
-                                                                             'aud' => 'APP_ID',
-                                                                             'sub' => '100000000000000000000',
-                                                                             'email' => 'john@example.com',
-                                                                             'email_verified' => true,
-                                                                             'at_hash' => 'HK6E_P6Dh8Y93mRNtsDB1Q',
-                                                                             'iss' => 'accounts.google.com',
-                                                                             'iat' => 1_496_117_119,
-                                                                             'exp' => 1_496_120_719
-                                                                           },
-                                                                           'raw_info' => {
-                                                                             'sub' => '100000000000000000000',
+                                                                           'provider' => 'google_oauth2',
+                                                                           'uid' => '111111111111111111',
+                                                                           'info' => {
+                                                                             'nick_name' => 'John Smith',
                                                                              'name' => 'John Smith',
-                                                                             'given_name' => 'John',
-                                                                             'family_name' => 'Smith',
-                                                                             'profile' => 'https://plus.google.com/+JohnSmith',
-                                                                             'picture' => 'https://lh4.googleusercontent.com/photo.jpg?sz=50',
                                                                              'email' => 'john@example.com',
-                                                                             'email_verified' => 'true',
-                                                                             'locale' => 'en',
-                                                                             'hd' => 'company.com'
+                                                                             'first_name' => 'John',
+                                                                             'last_name' => 'Smith',
+                                                                             'image' => 'https://lh4.googleusercontent.com/photo.jpg',
+                                                                             'urls' => {
+                                                                               'google' => 'https://plus.google.com/+JohnSmith'
+                                                                             }
+                                                                           },
+                                                                           'credentials' => {
+                                                                             'token' => 'TOKEN',
+                                                                             'refresh_token' => 'REFRESH_TOKEN',
+                                                                             'expires_at' => 1_496_120_719,
+                                                                             'expires' => true
+                                                                           },
+                                                                           'extra' => {
+                                                                             'id_token' => 'ID_TOKEN',
+                                                                             'id_info' => {
+                                                                               'azp' => 'APP_ID',
+                                                                               'aud' => 'APP_ID',
+                                                                               'sub' => '100000000000000000000',
+                                                                               'email' => 'john@example.com',
+                                                                               'email_verified' => true,
+                                                                               'at_hash' => 'HK6E_P6Dh8Y93mRNtsDB1Q',
+                                                                               'iss' => 'accounts.google.com',
+                                                                               'iat' => 1_496_117_119,
+                                                                               'exp' => 1_496_120_719
+                                                                             },
+                                                                             'raw_info' => {
+                                                                               'sub' => '100000000000000000000',
+                                                                               'name' => 'John Smith',
+                                                                               'given_name' => 'John',
+                                                                               'family_name' => 'Smith',
+                                                                               'profile' => 'https://plus.google.com/+JohnSmith',
+                                                                               'picture' => 'https://lh4.googleusercontent.com/photo.jpg?sz=50',
+                                                                               'email' => 'john@example.com',
+                                                                               'email_verified' => 'true',
+                                                                               'locale' => 'en',
+                                                                               'hd' => 'company.com'
+                                                                             }
                                                                            }
-                                                                         }
-                                                                       })
+                                                                         })
       request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
-      get :omniauth, params: { provider: "google_oauth2" }
+      get :omniauth, params: { provider: 'google_oauth2' }
       expect(response).to redirect_to(root_url)
     end
 
@@ -99,7 +105,7 @@ RSpec.describe SessionsController, type: :controller do
       User.login_with_third_party(req_evn)
     end
 
-    it 'redirects to login_path when omniauth.auth is nil' do 
+    it 'redirects to login_path when omniauth.auth is nil' do
       user = instance_double(User, id: nil, name: 'John', email: 'john@example.com', provider: 'google_oauth2', uid: '123')
       req_evn = { 'omniauth.auth' => { 'uid' => '123', 'provider' => 'google', 'info' => { 'name' => 'John', 'email' => 'john@example.com' } } }
       User.login_with_third_party(req_evn)
@@ -109,6 +115,5 @@ RSpec.describe SessionsController, type: :controller do
       get :omniauth, params: { provider: 'google_oauth2' }
       expect(response).to redirect_to(login_path)
     end
-
   end
 end
