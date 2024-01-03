e367e8d95d2a6f9945b5a2516cfa43753a36ef01
Your Name <huyhuhyhoa@gmail.com>
2024-01-03 16:47:18 +0700

    ref #1 DEV: fix rspec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,13 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  it 'should have a valid factory' do
-    user = FactoryBot.create(:user)
-    expect(user).to be_valid
-  end
-end
-
 RSpec.describe SessionsController, type: :controller do
   it 'should have a valid factory' do
     user = FactoryBot.create(:user)
