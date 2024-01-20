32055191a90f05e5869316a384eedb66bcc86016
Your Name <huyhuhyhoa@gmail.com>
2024-01-20 09:55:29 +0700

    ref #4 DEV:write controller and html

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,10 +1,9 @@
 class ChatRoomsController < ApplicationController
-  before_action :logged_in_user, only: %i[index create show]
-  
+  before_action :logged_in_user, only: %i[index create show destroy]
+  before_action :find_chat_room, only: %i[destroy]
+
   def index
-    @chat_room = ChatRoom.new
-    @chat_rooms = ChatRoom.all
-    @user_chats = User.all_except(current_user)
+    show_users_and_rooms
   end
 
   def create
@@ -14,14 +13,44 @@ class ChatRoomsController < ApplicationController
 
   def show
     @single_room = ChatRoom.find(params[:id])
-    current_user.remembers.find_by(chat_room_id: @single_room.id)
-
-    @chat_rooms = ChatRoom.all
-    @user_chats = User.all_except(current_user)
-    @chat_room = ChatRoom.new
-    @message = Message.new
-
-    @messages = @single_room.messages.order(created_at: :asc)
+    show_users_and_rooms
+    show_message
     render 'index'
+  end
+
+  def destroy
+    begin
+      @chat_room.destroy!
+      flash[:success] = 'Chat Room deleted'
+    rescue ActiveRecord::RecordNotDestroyed
+      flash[:error] = 'Chat Room could not be deleted'
+    end
+    redirect_to chat_rooms_path || root_url
+  end
+
+  private
+
+  def find_chat_room
+    @chat_room = ChatRoom.find(params[:id])
+  end
+
+  def show_message
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+  end
+
+  def show_users_and_rooms
+    @chat_rooms = find_all_chat_room
+    @chat_room = ChatRoom.new
+    @user_chats = find_users
+  end
+
+  def find_users
+    all_users = current_user.following + current_user.followers
+    users = all_users.uniq - [current_user] if all_users.uniq
+  end
+
+  def find_all_chat_room
+    chat_rooms = ChatRoom.where(id: current_user.remembers) 
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,6 @@ class MessagesController < ApplicationController
   before_action :logged_in_user, only: %i[create]
   def create
     @message = current_user.messages.create(message: msg_params[:message], chat_room_id: params[:chat_room_id])
-    
   end
 
   private
