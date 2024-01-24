b6941457c29accc4bd932988a56c071519e3272c
Your Name <huyhuhyhoa@gmail.com>
2024-01-24 13:52:02 +0700

    rè

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./controllers"
+import "controllers"
 // import "./controllers/hello_controller.js"
 
 import "jquery"
@@ -9,4 +9,4 @@ import "popper"
 import "bootstrap"
 import 'jquery-ujs'
 
-import "./channels"
+import "channels"
