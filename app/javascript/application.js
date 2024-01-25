5d088024143c2f014836fcf99af60f5fd1f14d75
Your Name <huyhuhyhoa@gmail.com>
2024-01-25 13:36:19 +0700

    ref #3 DEV:fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "controllers"
 import "@hotwired/turbo-rails"
-import "./controllers"
-// import "./controllers/hello_controller.js"
 
 import "jquery"
 import "jquery_ujs"
-import "popper"
-import "bootstrap"
-import {} from 'jquery-ujs'
 
+import "bootstrap"
+
+
+import "channels"
